2e585201c3493586d2fb3037115a4828f26b4ae2
manukminasyan <manuk.minasyan1@gmail.com>
2023-07-19 18:57:17 +0400

    Vitepress gitignore cache

--- docs/.vitepress/cache/deps/tailwindcss_colors.js
+++
@@ -1,7 +0,0 @@
-import {
-  require_colors
-} from "./chunk-SJYOEBSY.js";
-import "./chunk-MLQ4NW6D.js";
-import "./chunk-CQXHTUV2.js";
-export default require_colors();
-//# sourceMappingURL=tailwindcss_colors.js.map
--- docs/.vitepress/cache/deps/tailwindcss_defaultTheme.js
+++
@@ -1,6 +0,0 @@
-import {
-  require_defaultTheme
-} from "./chunk-NVLXQ6EX.js";
-import "./chunk-CQXHTUV2.js";
-export default require_defaultTheme();
-//# sourceMappingURL=tailwindcss_defaultTheme.js.map
