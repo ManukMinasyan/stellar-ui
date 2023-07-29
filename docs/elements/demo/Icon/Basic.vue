41bb422d12e0b3c489598b5b5ee3ba645265f371
manukminasyan <manuk.minasyan1@gmail.com>
2023-07-29 22:56:36 +0400

    Icon library instructions

--- docs/elements/demo/Icon/Basic.vue
+++ docs/elements/demo/Icon/Basic.vue
@@ -1,3 +1,6 @@
 <template>
-  <SIcon name="icon-[heroicons--light-bulb]" />
+  <s-icon name="icon-[heroicons--light-bulb]" />
+  <s-icon name="icon-[ph--alarm-duotone]" />
+  <s-icon name="icon-[fluent-emoji-flat--alarm-clock]" />
+  <s-icon name="icon-[carbon--edit-off]" />
 </template>
