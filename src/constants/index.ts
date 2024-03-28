716e37804601c65902e897f9ea31c95b6bb6a88e
manukminasyan <manuk.minasyan1@gmail.com>
2024-03-28 04:43:12 +0400

    Export colors

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,7 @@
 import * as AppConfig from './app.config'
+import * as colors from './colors'
 
 export {
     AppConfig,
+    colors,
 }
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-import { App } from 'vue'
+import {App} from 'vue'
 import * as components from './components'
 
-function install (app: App) {
-  for (const key in components) {
-    // @ts-ignore
-    app.component(key, components[key])
-  }
+function install(app: App) {
+    for (const key in components) {
+        // @ts-ignore
+        app.component(key, components[key])
+    }
 }
 
 import './assets/main.scss'
 
-export default { install }
+export default {install}
 
 export * from './components'
 export * from './constants'
