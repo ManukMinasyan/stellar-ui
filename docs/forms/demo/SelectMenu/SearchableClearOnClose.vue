431daf31f30cdcf6a4904db7860a2fb5a7c29867
manukminasyan <manuk.minasyan1@gmail.com>
2024-06-23 15:05:46 +0400

    fix(InputMenu/SelectMenu): invalid label with value-attribute and async search

--- docs/forms/demo/SelectMenu/SearchableClearOnClose.vue
+++ docs/forms/demo/SelectMenu/SearchableClearOnClose.vue
@@ -1,5 +1,11 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+const person = ref(null)
+</script>
 <template>
   <SSelectMenu
+      v-model="person"
       clear-search-on-close
       class="w-full lg:w-48"
       placeholder="Select a person"
