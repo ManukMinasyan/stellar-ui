504ccfb6401444bb19895967c0fbebe839ed2db7
manukminasyan <manuk.minasyan1@gmail.com>
2024-12-29 10:19:19 +0100

    feat: add locale management utilities and defineLocale composable

--- src/composables/useLocale.ts
+++ src/composables/useLocale.ts
@@ -1,11 +1,11 @@
 import { computed, inject, ref } from 'vue'
 import type { InjectionKey, Ref } from 'vue'
-import type { Locale } from '../types/locale'
+import type { Locale } from '@/types'
 import { buildLocaleContext } from '@/utils/locale'
 import en from '../locale/en'
 import { createSharedComposable } from '@vueuse/core'
 
-export const localeContextInjectionKey: InjectionKey<Ref<Locale | undefined>> = Symbol('nuxt-ui.locale-context')
+export const localeContextInjectionKey: InjectionKey<Ref<Locale | undefined>> = Symbol('stellar-ui.locale-context')
 
 const _useLocale = (localeOverrides?: Ref<Locale | undefined>) => {
     const locale = localeOverrides || inject(localeContextInjectionKey, ref())!
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,2 @@
 export * from '../components/Alert.vue'
+export * from './locale'
