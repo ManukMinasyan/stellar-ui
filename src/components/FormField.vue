00607f95c1c66546382cd70b9fcaae1b4e387017
manukminasyan <manuk.minasyan1@gmail.com>
2025-01-20 12:56:46 +0100

    feat: add FormField component with customizable slots and styles

---
+++ src/components/FormField.vue
@@ -0,0 +1,124 @@
+<script lang="ts">
+import type { VariantProps } from 'tailwind-variants'
+import type { AppConfig } from '@/types/config'
+import _appConfig from '#build/app.config'
+import theme from '#build/ui/form-field'
+import { tv } from '@/utils/tv'
+
+const appConfigFormField = _appConfig as AppConfig & { ui: { formField: Partial<typeof theme> } }
+
+const formField = tv({ extend: tv(theme), ...(appConfigFormField.ui?.formField || {}) })
+
+type FormFieldVariants = VariantProps<typeof formField>
+
+export interface FormFieldProps {
+  /**
+   * The element or component this component should render as.
+   * @defaultValue 'div'
+   */
+  as?: any
+  /** The name of the FormField. Also used to match form errors. */
+  name?: string
+  /** A regular expression to match form error names. */
+  errorPattern?: RegExp
+  label?: string
+  description?: string
+  help?: string
+  error?: string | boolean
+  hint?: string
+  size?: FormFieldVariants['size']
+  required?: boolean
+  eagerValidation?: boolean
+  validateOnInputDelay?: number
+  class?: any
+  ui?: Partial<typeof formField.slots>
+}
+
+export interface FormFieldSlots {
+  label(props: { label?: string }): any
+  hint(props: { hint?: string }): any
+  description(props: { description?: string }): any
+  help(props: { help?: string }): any
+  error(props: { error?: string | boolean }): any
+  default(props: { error?: string | boolean }): any
+}
+
+</script>
+
+<script setup lang="ts">
+import { computed, ref, inject, provide, type Ref, useId } from 'vue'
+import { Primitive, Label } from 'reka-ui'
+import { formFieldInjectionKey, inputIdInjectionKey } from '@/composables/useFormField'
+import type { FormError, FormFieldInjectedOptions } from '@/types/form'
+
+const props = defineProps<FormFieldProps>()
+const slots = defineSlots<FormFieldSlots>()
+
+const ui = computed(() => formField({
+  size: props.size,
+  required: props.required
+}))
+
+const formErrors = inject<Ref<FormError[]> | null>('form-errors', null)
+
+const error = computed(() => props.error || formErrors?.value?.find(error => error.name === props.name || (props.errorPattern && error.name.match(props.errorPattern)))?.message)
+
+const id = ref(useId())
+// Copies id's initial value to bind aria-attributes such as aria-describedby.
+// This is required for the RadioGroup component which unsets the id value.
+const ariaId = id.value
+
+provide(inputIdInjectionKey, id)
+
+provide(formFieldInjectionKey, computed(() => ({
+  error: error.value,
+  name: props.name,
+  size: props.size,
+  eagerValidation: props.eagerValidation,
+  validateOnInputDelay: props.validateOnInputDelay,
+  errorPattern: props.errorPattern,
+  hint: props.hint,
+  description: props.description,
+  ariaId
+}) as FormFieldInjectedOptions<FormFieldProps>))
+</script>
+
+<template>
+  <Primitive :as="as" :class="ui.root({ class: [props.class, props.ui?.root] })">
+    <div :class="ui.wrapper({ class: props.ui?.wrapper })">
+      <div v-if="label || !!slots.label" :class="ui.labelWrapper({ class: props.ui?.labelWrapper })">
+        <Label :for="id" :class="ui.label({ class: props.ui?.label })">
+          <slot name="label" :label="label">
+            {{ label }}
+          </slot>
+        </Label>
+        <span v-if="hint || !!slots.hint" :id="`${ariaId}-hint`" :class="ui.hint({ class: props.ui?.hint })">
+          <slot name="hint" :hint="hint">
+            {{ hint }}
+          </slot>
+        </span>
+      </div>
+
+      <p v-if="description || !!slots.description" :id="`${ariaId}-description`" :class="ui.description({ class: props.ui?.description })">
+        <slot name="description" :description="description">
+          {{ description }}
+        </slot>
+      </p>
+    </div>
+
+    <div :class="[(label || !!slots.label || description || !!slots.description) && ui.container({ class: props.ui?.container })]">
+      <slot :error="error" />
+
+      <p v-if="(typeof error === 'string' && error) || !!slots.error" :id="`${ariaId}-error`" :class="ui.error({ class: props.ui?.error })">
+        <slot name="error" :error="error">
+          {{ error }}
+        </slot>
+      </p>
+      <p v-else-if="help || !!slots.help" :class="ui.help({ class: props.ui?.help })">
+        <slot name="help" :help="help">
+          {{ help }}
+        </slot>
+      </p>
+    </div>
+  </Primitive>
+</template>
---
+++ src/theme/form-field.ts
@@ -0,0 +1,30 @@
+export default {
+    slots: {
+        root: '',
+        wrapper: '',
+        labelWrapper: 'flex content-center items-center justify-between',
+        label: 'block font-medium text-[var(--ui-text)]',
+        container: 'mt-1 relative',
+        description: 'text-[var(--ui-text-muted)]',
+        error: 'mt-2 text-[var(--ui-error)]',
+        hint: 'text-[var(--ui-text-muted)]',
+        help: 'mt-2 text-[var(--ui-text-muted)]'
+    },
+    variants: {
+        size: {
+            xs: { root: 'text-xs' },
+            sm: { root: 'text-xs' },
+            md: { root: 'text-sm' },
+            lg: { root: 'text-sm' },
+            xl: { root: 'text-base' }
+        },
+        required: {
+            true: {
+                label: `after:content-['*'] after:ms-0.5 after:text-[var(--ui-error)]`
+            }
+        }
+    },
+    defaultVariants: {
+        size: 'md'
+    }
+}
