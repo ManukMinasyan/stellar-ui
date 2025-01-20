88447645a04872e95ac6f1cb75b43c57ae14f0a4
manukminasyan <manuk.minasyan1@gmail.com>
2025-01-20 10:56:46 +0100

    feat: implement useFormField composable for enhanced form field management

---
+++ src/composables/useFormField.ts
@@ -0,0 +1,64 @@
+import { inject, computed, type InjectionKey, type Ref, type ComputedRef } from 'vue'
+import { type UseEventBusReturn, useDebounceFn } from '@vueuse/core'
+import type { FormFieldProps } from '../types'
+import type { FormEvent, FormInputEvents, FormFieldInjectedOptions, FormInjectedOptions } from '../types/form'
+import type { GetObjectField } from '@/types/utils'
+
+type Props<T> = {
+    id?: string
+    name?: string
+    size?: GetObjectField<T, 'size'>
+    color?: GetObjectField<T, 'color'>
+    highlight?: boolean
+    disabled?: boolean
+}
+
+export const formOptionsInjectionKey: InjectionKey<ComputedRef<FormInjectedOptions>> = Symbol('nuxt-ui.form-options')
+export const formBusInjectionKey: InjectionKey<UseEventBusReturn<FormEvent<any>, string>> = Symbol('nuxt-ui.form-events')
+export const formFieldInjectionKey: InjectionKey<ComputedRef<FormFieldInjectedOptions<FormFieldProps>>> = Symbol('nuxt-ui.form-field')
+export const inputIdInjectionKey: InjectionKey<Ref<string | undefined>> = Symbol('nuxt-ui.input-id')
+export const formInputsInjectionKey: InjectionKey<Ref<Record<string, { id?: string, pattern?: RegExp }>>> = Symbol('nuxt-ui.form-inputs')
+export const formLoadingInjectionKey: InjectionKey<Readonly<Ref<boolean>>> = Symbol('nuxt-ui.form-loading')
+
+export function useFormField<T>(props?: Props<T>, opts?: { bind?: boolean, deferInputValidation?: boolean }) {
+    const formOptions = inject(formOptionsInjectionKey, undefined)
+    const formBus = inject(formBusInjectionKey, undefined)
+    const formField = inject(formFieldInjectionKey, undefined)
+    const formInputs = inject(formInputsInjectionKey, undefined)
+    const inputId = inject(inputIdInjectionKey, undefined)
+
+    if (formField && inputId) {
+        if (opts?.bind === false) {
+            // Removes for="..." attribute on label for RadioGroup and alike.
+            inputId.value = undefined
+        } else if (props?.id) {
+            // Updates for="..." attribute on label if props.id is provided.
+            inputId.value = props?.id
+        }
+
+        if (formInputs && formField.value.name && inputId.value) {
+            formInputs.value[formField.value.name] = { id: inputId.value, pattern: formField.value.errorPattern }
+        }
+    }
+
+    return {
+        id: computed(() => props?.id ?? inputId?.value),
+        name: computed(() => props?.name ?? formField?.value.name),
+        size: computed(() => props?.size ?? formField?.value.size),
+        color: computed(() => formField?.value.error ? 'error' : props?.color),
+        highlight: computed(() => formField?.value.error ? true : props?.highlight),
+        disabled: computed(() => formOptions?.value.disabled || props?.disabled),
+        ariaAttrs: computed(() => {
+            if (!formField?.value) return
+
+            const descriptiveAttrs = ['error' as const, 'hint' as const, 'description' as const]
+                .filter(type => formField?.value?.[type])
+                .map(type => `${formField?.value.ariaId}-${type}`) || []
+
+            return {
+                'aria-describedby': descriptiveAttrs.join(' '),
+                'aria-invalid': !!formField?.value.error
+            }
+        })
+    }
+}
