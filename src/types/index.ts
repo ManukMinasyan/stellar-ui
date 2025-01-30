cb006131d24e1f587232d7f524b79186599a3ed4
manukminasyan <manuk.minasyan1@gmail.com>
2025-01-30 21:22:37 +0400

    feat: export Button component from types index

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,5 @@
 export * from '../components/AvatarGroup.vue'
 export * from '../components/Avatar.vue'
 export * from '../components/Alert.vue'
+export * from '../components/Button.vue'
 export * from './locale'
